e0e69cc129124ffdce9d30cb101d98ba5bf7e7c1
karthikeyan-sam <2k21cse065@kiot.ac.in>
2024-08-25 21:07:12 +0530

    add all file

---
+++ src/main/java/com/project/Chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.project.Chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
